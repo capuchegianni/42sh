02d2587d2fb5f02b274802f7fef9099399faf4aa
Gianni Henriques <gianni.henriques@epitech.eu>
2023-04-22 23:56:50 +0200

    [~] fix init shell function

--- src/inits/init_shell.c
+++ src/inits/init_shell.c
@@ -15,5 +15,7 @@ shell_t *init_shell(shell_t *my_shell)
     my_shell->row = 0;
     my_shell->return_val = 0;
     my_shell->history = NULL;
+    my_shell->alias = NULL;
+    my_shell->cmd = NULL;
     return (my_shell);
 }
