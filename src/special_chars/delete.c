5fc5dc024989e154a0ef0352331c88a2b6fcd229
ElouanR <elouan.rigomont@epitech.eu>
2023-04-27 20:21:12 +0200

    [+] manage delete

--- src/special_chars/delete.c
+++ src/special_chars/delete.c
@@ -7,14 +7,34 @@
 
 #include "project.h"
 
+void update_prompt_cmd(shell_t *shell)
+{
+    for (int i = shell->cursor_pos; i < shell->len; i++)
+        printf("\033[C");
+    for (int i = shell->len; i >= 0; i--)
+        printf("\b \b");
+    printf("%s", shell->buffer);
+    for (int i = shell->len; i > shell->cursor_pos; i--)
+        printf("\b");
+}
+
+void update_buffer(shell_t *shell)
+{
+    int i = shell->cursor_pos;
+
+    for (; shell->buffer[i]; i++)
+        shell->buffer[i] = shell->buffer[i + 1];
+    shell->buffer[i] = '\0';
+}
+
 int my_delete(int c, shell_t *shell)
 {
     if ((c == 8 || c == 127)) {
-        if (shell->len > 0) {
-            printf("\b \b");
-            shell->buffer[shell->len] = '\0';
+        if (shell->cursor_pos > 0) {
             shell->len--;
             shell->cursor_pos--;
+            update_buffer(shell);
+            update_prompt_cmd(shell);
         }
         return (1);
     }
