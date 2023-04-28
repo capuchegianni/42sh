a9bcb0c2627e4c84ac59b5dc8a6f01ec5fdb365c
ElouanR <elouan.rigomont@epitech.eu>
2023-04-28 14:30:04 +0200

    [+] alias handling

---
+++ src/commands/alias/exec_alias.c
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2023
+** 42sh
+** File description:
+** exec_alias
+*/
+
+#include "project.h"
+
+void change_cmd(shell_t *shell, alias_t *current)
+{
+    char *command = "";
+
+    if (!shell->first_alias)
+        shell->first_alias = my_strdup(shell->cmd[0]);
+    command = my_strcat(current->command, " ");
+    for (int i = 1; shell->cmd[i]; i++) {
+        command = my_strcat(command, shell->cmd[i]);
+        command = my_strcat(command, " ");
+    }
+    shell->cmd = my_wordarray(command, " ");
+    if (!shell->cmd)
+        exit(84);
+    current = shell->alias;
+}
+
+int exec_alias(shell_t *shell)
+{
+    alias_t *current = shell->alias;
+
+    while (current) {
+        if (shell->first_alias &&
+        strcmp(shell->cmd[0], shell->first_alias) == 0) {
+            dprintf(2, "Alias loop.\n");
+            shell->return_val = 1;
+            return (1);
+        }
+        if (strcmp(current->name, shell->cmd[0]) == 0) {
+            change_cmd(shell, current);
+        } else
+            current = current->next;
+        shell->return_val = 0;
+    }
+    return (0);
+}
--- src/parsing/check_cmd.c
+++ src/parsing/check_cmd.c
@@ -15,25 +15,20 @@ int check_alias_cmd(shell_t *shell)
     }
     if (strcmp(shell->cmd[0], "alias") == 0 && my_tablen(shell->cmd) == 2) {
         print_specify_alias(shell);
+        shell->return_val = 0;
         return (1);
     }
     if (strcmp(shell->cmd[0], "alias") == 0 && my_tablen(shell->cmd) == 1) {
         print_alias(shell);
+        shell->return_val = 0;
         return (1);
     }
+    shell->first_alias = NULL;
     return (0);
 }
 
-int check_cmd(shell_t *shell)
+int check_cmd_split(shell_t *shell)
 {
-    if (cd_cmd(shell) == 1)
-        return (1);
-    if (strcmp(shell->cmd[0], "exit") == 0)
-        return (1);
-    if (print_env(shell) == 1)
-        return (1);
-    if (my_setenv(shell) == 1)
-        return (1);
     if (my_unsetenv(shell) == 1)
         return (1);
     if (strcmp(shell->cmd[0], "history") == 0 ||
@@ -44,6 +39,23 @@ int check_cmd(shell_t *shell)
     if (strcmp(shell->cmd[0], "unalias") == 0)
         return (1);
     if (strcmp(shell->cmd[0], "echo") == 0)
+        return (1);
+    return (0);
+}
+
+int check_cmd(shell_t *shell)
+{
+    if (exec_alias(shell) == 1)
+        return (1);
+    if (cd_cmd(shell) == 1)
+        return (1);
+    if (strcmp(shell->cmd[0], "exit") == 0)
+        return (1);
+    if (print_env(shell) == 1)
+        return (1);
+    if (my_setenv(shell) == 1)
+        return (1);
+    if (check_cmd_split(shell) == 1)
         return (1);
     return (0);
 }
