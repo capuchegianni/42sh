7e36f368549067e61ef9847ae642a26a69f139ac
Arthur Bichara <arthur.bichara@epitech.eu>
2023-05-09 15:32:59 +0200

    [~] change struct of the file and the fct in it

--- src/parsing/redirection/redirection.c
+++ src/parsing/redirection/redirection.c
@@ -2,7 +2,7 @@
 ** EPITECH PROJECT, 2023
 ** 42sh
 ** File description:
-** check_cmd
+** redirection
 */
 
 #include "project.h"
@@ -11,10 +11,8 @@ static int find_spe_char(shell_t *shell, int i)
 {
     int red = 0;
 
-    my_printf("la05\n");
     if (my_cmp(shell->cmd[i], ">") == 0)
         red = 1;
-    my_printf("la06\n");
     if (my_cmp(shell->cmd[i], ">>") == 0)
         red = 2;
     if (my_cmp(shell->cmd[i], "<") == 0)
@@ -22,17 +20,6 @@ static int find_spe_char(shell_t *shell, int i)
     if (my_cmp(shell->cmd[i], "<<") == 0)
         red = 4;
     return red;
-}
-
-void close_red(shell_t *shell, int index)
-{
-
-}
-
-int open_red(shell_t *shell)
-{
-
-    return 1;
 }
 
 static int check_command(shell_t *shell, int index)
@@ -43,7 +30,7 @@ static int check_command(shell_t *shell, int index)
         return 0;
     if (my_strlen(shell->cmd[index + 1]) == 0)
         return 0;
-    return open_red(shell);
+    return open_red(shell, index);
 }
 
 int redirection(shell_t *shell)
@@ -51,18 +38,14 @@ int redirection(shell_t *shell)
     int nb_red = 0;
     int index = 0;
 
-    my_printf("la00\n");
     for (int i = 0; shell->cmd[i] != NULL; i++) {
         if (find_spe_char(shell, i) != 0) {
-            my_printf("la04\n");
             index = i;
             nb_red++;
         }
     }
-    my_printf("la01\n");
     if (nb_red == 1)
         if (check_command(shell, index))
             return 1;
-    my_printf("la02\n");
     return 0;
 }
